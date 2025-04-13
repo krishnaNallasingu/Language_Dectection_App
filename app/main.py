9140bf3ad8e6b01c99a90a7661cd8271f63acf04
krishnaNallasingu <jagan.krishna@students.iiit.ac.in>
2025-04-13 10:24:30 +0530

    Updated main.py

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ class PredictionOut(BaseModel):
 
 @app.get("/")
 def home():
-    return {"health_check": "OK", "model_version": model_version}
+    return {"health_check": "OK", "model_version": model_version, "Is it Working": "Yes :)"}
 
 
 @app.post("/predict", response_model=PredictionOut)
